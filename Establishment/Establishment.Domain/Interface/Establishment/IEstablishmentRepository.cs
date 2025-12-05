ae65bedc84d07c5adfd375c781fdea7281e37b63
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-12-05 17:10:37 -0300

    feat: criando a interface: IEstablishmentRepository

---
+++ Establishment/Establishment.Domain/Interface/Establishment/IEstablishmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Establishment.Domain.Interface.Establishment
+{
+    public interface IEstablishmentRepository
+    {
+    }
+}
