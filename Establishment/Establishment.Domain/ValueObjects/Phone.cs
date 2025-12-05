963981a1f0d1562a21293891e0e5810126e1bad3
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-12-05 16:26:59 -0300

    feat: criando o value object do telefone

---
+++ Establishment/Establishment.Domain/ValueObjects/Phone.cs
@@ -0,0 +1,31 @@
+﻿using Establishment.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Establishment.Domain.ValueObjects
+{
+    public sealed record Phone
+    {
+        private string Value { get; }
+
+        private Phone(string value)
+        {
+            Value = value;
+        }
+
+        public static Result<Phone> Create(string rawPhone)
+        {
+            if (string.IsNullOrWhiteSpace(rawPhone))
+            {
+                return Result.Failure<Phone>("Phone number cannot be null or empty.");
+            }
+            var digitsOnlyPhone = new string(rawPhone.Where(char.IsDigit).ToArray());
+            if (digitsOnlyPhone.Length < 10 || digitsOnlyPhone.Length > 11)
+            {
+                return Result.Failure<Phone>("Phone number must have 10 or 11 digits.");
+            }
+            return Result.Success(new Phone(digitsOnlyPhone));
+        }
+    }
+}
