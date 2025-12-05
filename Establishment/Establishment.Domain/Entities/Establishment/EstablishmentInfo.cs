10222b448590e5f7dc59fc4ae6c62cd82c4bb962
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-12-05 19:32:28 -0300

    feat: adicioanndo um aggregator - EstablishmentInfo

--- Establishment/Establishment.Domain/Entities/Establishment/EstablishmentInfo.cs
+++ Establishment/Establishment.Domain/Entities/Establishment/EstablishmentInfo.cs
@@ -1,4 +1,5 @@
-﻿using Establishment.Domain.ValueObjects;
+﻿using Establishment.Domain.Common;
+using Establishment.Domain.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,9 +14,9 @@ namespace Establishment.Domain.Entities.Establishment
         public string Address { get; private set; }
         public Phone Phone { get; private set; }
         public NumberOfCarPlaces NumberOfCarPlaces { get; private set; }
-        public NumberOfMotorcyclelaces NumberOfMotorcyclelaces { get; private set; }
+        public NumberOfMotorcyclePlaces NumberOfMotorcyclelaces { get; private set; }
 
-        public EstablishmentInfo(int id, string name, Cnpj cnpj, string address, Phone phone, NumberOfCarPlaces numberOfCarPlaces, NumberOfMotorcyclelaces numberOfMotorcyclelaces)
+        public EstablishmentInfo(int id, string name, Cnpj cnpj, string address, Phone phone, NumberOfCarPlaces numberOfCarPlaces, NumberOfMotorcyclePlaces numberOfMotorcyclelaces)
         {
             Id = id;
             Name = name;
@@ -25,5 +26,29 @@ namespace Establishment.Domain.Entities.Establishment
             NumberOfCarPlaces = numberOfCarPlaces;
             NumberOfMotorcyclelaces = numberOfMotorcyclelaces;
         }
+
+        public EstablishmentInfo(string name, Cnpj cnpj, string address, Phone phone, NumberOfCarPlaces numberOfCarPlaces, NumberOfMotorcyclePlaces numberOfMotorcyclelaces)
+        {
+            Name = name;
+            Cnpj = cnpj;
+            Address = address;
+            Phone = phone;
+            NumberOfCarPlaces = numberOfCarPlaces;
+            NumberOfMotorcyclelaces = numberOfMotorcyclelaces;
+        }
+
+        public static Result<EstablishmentInfo> Create(string name, Cnpj cnpj, string address, Phone phone, NumberOfCarPlaces numberOfCarPlaces, NumberOfMotorcyclePlaces numberOfMotorcyclelaces)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return Result.Failure<EstablishmentInfo>("Name cannot be null or empty.");
+            }
+            if (string.IsNullOrWhiteSpace(address))
+            {
+                return Result.Failure<EstablishmentInfo>("Address cannot be null or empty.");
+            }
+            return Result.Success(new EstablishmentInfo(name, cnpj, address, phone, numberOfCarPlaces, numberOfMotorcyclelaces));
+        }
+
     }
 }
