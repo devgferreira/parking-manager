2a72049e82ea7c8244a91633fd5ca1ff1e62ef23
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-12-05 14:32:45 -0300

    feat: criando o valueObject do Cnpj

---
+++ Establishment/Establishment.Domain/ValueObjects/Cnpj.cs
@@ -0,0 +1,33 @@
+﻿using Establishment.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Establishment.Domain.ValueObjects
+{
+    public sealed record Cnpj
+    {
+        private Cnpj(string value)
+        {
+            Value = value;
+        }
+        private string Value { get; }
+
+        public static Result<Cnpj> Create(string rawCnpj)
+        {
+            if(string.IsNullOrWhiteSpace(rawCnpj))
+            {
+                return Result.Failure<Cnpj>("CNPJ cannot be null or empty.");
+            }
+            var digitsOnlyCnpj = new string(rawCnpj.Where(char.IsDigit).ToArray());
+
+            if(digitsOnlyCnpj.Length != 14)
+            {
+                return Result.Failure<Cnpj>("CNPJ must have exactly 14 digits.");
+            }
+
+            return Result.Success(new Cnpj(digitsOnlyCnpj));
+        }
+
+    }
+}
