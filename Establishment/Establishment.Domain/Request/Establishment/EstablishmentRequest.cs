3d4bc5118eeab17b595a612b5b46a7721cff4782
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-12-05 19:17:26 -0300

    feat: criando o request para busca - EstablishmentRequest

---
+++ Establishment/Establishment.Domain/Request/Establishment/EstablishmentRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Establishment.Domain.Request.Establishment
+{
+    public class EstablishmentRequest
+    {
+        public int? Id { get; private set; }
+        public string? Name { get; private set; }
+        public string? Cnpj { get; private set; }
+        public string? Phone { get; private set; }
+    }
+}
