88731eb6f175076a2fb815a3b30f48414893f9fd
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-12-08 11:04:11 -0300

    feat: criando o DTO: EstablishmentDTO

---
+++ Establishment/Establishment.Application/DTO/Establishment/EstablishmentDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Establishment.Application.DTO.Establishment
+{
+    public class EstablishmentDTO
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Cnpj { get; private set; }
+        public string Address { get; private set; }
+        public string Phone { get; private set; }
+        public int NumberOfCarPlaces { get; private set; }
+        public int NumberOfMotorcyclelaces { get; private set; }
+    }
+}
