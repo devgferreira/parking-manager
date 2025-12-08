52800147fdb98e471641263ad52ce199895e864a
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-12-08 11:19:49 -0300

    faet: criando o DTO: EstablishmentCreateOrUpdateDTO

---
+++ Establishment/Establishment.Application/DTO/Establishment/EstablishmentCreateOrUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Establishment.Application.DTO.Establishment
+{
+    public class EstablishmentCreateOrUpdateDTO
+    {
+        public string Name { get; private set; }
+        public string Cnpj { get; private set; }
+        public string Address { get; private set; }
+        public string Phone { get; private set; }
+        public int NumberOfCarPlaces { get; private set; }
+        public int NumberOfMotorcyclelaces { get; private set; }
+    }
+}
