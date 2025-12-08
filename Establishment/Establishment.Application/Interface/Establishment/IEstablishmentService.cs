3931ee855ea24f3b7be9bd390133fe722733e00d
Gabriel <gabrielferreirasouto.com@gmail.com>
2025-12-08 12:13:51 -0300

    feat: criando a interface: IEstablishmentService

---
+++ Establishment/Establishment.Application/Interface/Establishment/IEstablishmentService.cs
@@ -0,0 +1,16 @@
+﻿using Establishment.Application.DTO.Establishment;
+using Establishment.Domain.Request.Establishment;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Establishment.Application.Interface.Establishment
+{
+    public interface IEstablishmentService
+    {
+        Task CreateEstablishment(EstablishmentCreateOrUpdateDTO establishment);
+        Task UpdateEstablishment(EstablishmentCreateOrUpdateDTO establishment);
+        Task DeleteEstablishment(int id);
+        Task<List<EstablishmentDTO>> SelectEstablishment(EstablishmentRequest request);
+    }
+}
